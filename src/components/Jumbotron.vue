94abb92f7217b164ec1dc6e6ab8e71f693d00538
h1ddengames <skarim3000@gmail.com>
2021-02-15 14:10:35 -0800

    Added bootstrap to the App

---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="jumbotron">
+    <b-jumbotron
+      header="Vue-Bootstrap-Docker"
+      lead="Vue.js 2 Using Bootstrap v4 Components running on Docker-Compose">
+      
+      <b-button variant="primary" href="#">More Info</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  //data() {},
+  //methods: {},
+};
+</script>
